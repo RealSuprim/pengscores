64f66dae16e3c5446ee5de4cce44886f3d052ff5
Suprim <10013614+RealSuprim@users.noreply.github.com>
2019-09-26 21:36:18 +0100

    Update axios.ts

--- app/utils/axios.ts
+++ app/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://api-football-v1.p.rapidapi.com/v2',
+  baseURL: 'https://server1.api-football.com/',
   headers: {
     'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
     'x-rapidapi-key': '<INSERT API KEY HERE>'
